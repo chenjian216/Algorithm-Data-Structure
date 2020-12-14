afa56bb36014a11367574c24758eba6ac6fa05bb
JianChen <jianchen@thoughtworks.com>
2020-12-14 22:15:53 +0800

    linear search v2

--- src/main/java/LinerSearch.java
+++ src/main/java/LinerSearch.java
@@ -1,9 +1,15 @@
 public class LinerSearch {
     private LinerSearch() {}
 
-    public static int search(int[] data, int target) {
+    public static int searchIntArray(int[] data, int target) {
         for(int i = 0; i< data.length; i++)
             if(data[i] == target) return i;
+        return -1;
+    }
+
+    public static <T> int search(T[] data, T target) {
+        for(int i = 0; i < data.length; i++)
+            if(data[i].equals(target)) return i;
         return -1;
     }
 }
--- src/test/java/LinerSearchTest.java
+++ src/test/java/LinerSearchTest.java
@@ -5,14 +5,20 @@ import static org.junit.Assert.*;
 public class LinerSearchTest {
 
     @Test
-    public void search() {
+    public void search_should_get_index_of_target() {
         int[] data = {12, 234, 32, 5, 2, 16, 95, 4};
-        assertEquals(LinerSearch.search(data, 16), 5);
+        assertEquals(LinerSearch.searchIntArray(data, 16), 5);
     }
 
     @Test
     public void search_should_return_negative_one_when_target_not_found() {
         int[] data = {12, 234, 32, 5, 2, 16, 95, 4};
-        assertEquals(LinerSearch.search(data, 100), -1);
+        assertEquals(LinerSearch.searchIntArray(data, 100), -1);
+    }
+
+    @Test
+    public void search_should_support_string_array() {
+        String[] data = {"12", "1234", "143"};
+        assertEquals(LinerSearch.search(data, "1234"), 1);
     }
 }
